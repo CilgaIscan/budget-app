d863d4e7a5283b69796f96c583a8f52383986b4a
cilga <cilgaiscan@gmail.com>
2023-01-10 23:23:54 +0100

    Add unit test for Expense service

---
+++ src/app/mock-data/expenses.ts
@@ -0,0 +1,20 @@
+export const Expenses = {
+  1: {
+    title: "Rent for January",
+    id: 1,
+    amount: "3000",
+    paid_at: "",
+    store: "",
+    payment_type: 1,
+    category: 1
+  },
+  2: {
+    title: "Grocery Shopping for W1",
+    id: 2,
+    amount: "3000",
+    paid_at: "",
+    store: "",
+    payment_type: 1,
+    category: 2
+  },
+}
--- src/app/pages/expenses/expense.service.spec.ts
+++ src/app/pages/expenses/expense.service.spec.ts
@@ -1,16 +1,84 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseService } from './expense.service';
+import { Expenses } from 'src/app/mock-data/expenses';
 
-xdescribe('ExpenseService', () => {
+describe('ExpenseService', () => {
   let service: ExpenseService;
+  let testingController: HttpTestingController;
+  const baseUrl = 'http://192.168.0.21:8000/api/expenses';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ExpenseService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be able to get all expenses', () => {
+    service.getAll().subscribe(exps => {
+      expect(exps).toBeTruthy();
+      expect(exps.length).toBe(2);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(Object.values(Expenses));
+  });
+
+  it('should be able to get expense by id', () => {
+    service.getById('1').subscribe(exp => {
+      expect(exp).toBeTruthy();
+      expect(exp.title).toBe('Rent for January');
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/1");
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(Expenses[1]);
+  });
+
+  it('should be able to create category', () => {
+    const newBody = {
+      title: 'Eating Out in Jeffs', 
+      amount: "100",
+      paid_at: "",
+      store: "Jeff's Burger",
+      payment_type: 2,
+      category: 2
+    };
+
+    service.create(newBody).subscribe(exp => {
+      expect(exp).toBeTruthy();
+      expect(exp.title).toBe(newBody.title);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('POST');
+    const response = {
+      ...Expenses[1],
+      title: newBody.title,
+      amount: newBody.amount,
+      store: newBody.store,
+      paid_at: newBody.paid_at,
+      payment_type: newBody.payment_type,
+      category: newBody.category
+    };
+    mockReq.flush(response);
+  });
+
+  it('should be able to delete category', () => {
+    service.delete('1').subscribe(exp => {
+      expect(exp).toBeFalsy();
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/1");
+    expect(mockReq.request.method).toBe('DELETE');
+    mockReq.flush('');
   });
 });

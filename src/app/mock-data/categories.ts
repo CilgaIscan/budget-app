56990de264593d5892ec0fd014fbc6a37888354c
cilga <cilgaiscan@gmail.com>
2023-01-10 23:07:32 +0100

    Add unit test for Category service

---
+++ src/app/mock-data/categories.ts
@@ -0,0 +1,14 @@
+export const Categories = {
+  1: {
+    name: "Rent",
+    id: 1,
+    icon: "cash",
+    color: "blue"
+  },
+  2: {
+    name: "Grocery",
+    id: 2,
+    icon: "cash",
+    color: "green"
+  },
+}
--- src/app/pages/categories/category.service.spec.ts
+++ src/app/pages/categories/category.service.spec.ts
@@ -1,16 +1,75 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CategoryService } from './category.service';
+import { Categories } from 'src/app/mock-data/categories';
 
-xdescribe('CategoryService', () => {
+describe('CategoryService', () => {
   let service: CategoryService;
+  let testingController: HttpTestingController;
+  const baseUrl = 'http://192.168.0.21:8000/api/categories';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CategoryService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be able to get all categories', () => {
+    service.getAll().subscribe(cs => {
+      expect(cs).toBeTruthy();
+      expect(cs.length).toBe(2);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(Object.values(Categories));
+  });
+
+  it('should be able to get category by id', () => {
+    service.getById('2').subscribe(c => {
+      expect(c).toBeTruthy();
+      expect(c.name).toBe('Grocery');
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/2");
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(Categories[2]);
+  });
+
+  it('should be able to create category', () => {
+    const newBody = { name: 'Eating Out', icon: 'hamburger', color: 'red' };
+
+    service.create(newBody).subscribe(c => {
+      expect(c).toBeTruthy();
+      expect(c.color).toBe(newBody.color);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('POST');
+    const response = {
+      ...Categories[1],
+      name: newBody.name,
+      icon: newBody.icon,
+      color: newBody.color,
+      id: 3
+    };
+    mockReq.flush(response);
+  });
+
+  it('should be able to delete category', () => {
+    service.delete('1').subscribe(c => {
+      expect(c).toBeFalsy();
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/1");
+    expect(mockReq.request.method).toBe('DELETE');
+    mockReq.flush('');
   });
 });

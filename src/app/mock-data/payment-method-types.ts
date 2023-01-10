7fba4d7af5aad70e0a511273dbf6480217e654f3
cilga <cilgaiscan@gmail.com>
2023-01-10 22:29:43 +0100

    Add unit test for PaymentMethodType service

---
+++ src/app/mock-data/payment-method-types.ts
@@ -0,0 +1,10 @@
+export const PaymentMethodTypes = {
+  1: {
+    name: "Cash",
+    id: 1
+  },
+  2: {
+    name: "Debit Card",
+    id: 2
+  }
+}
--- src/app/pages/payment-method-types/payment-method-type.service.spec.ts
+++ src/app/pages/payment-method-types/payment-method-type.service.spec.ts
@@ -1,16 +1,91 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PaymentMethodTypeService } from './payment-method-type.service';
+import { PaymentMethodTypes } from 'src/app/mock-data/payment-method-types';
 
-xdescribe('PaymentMethodTypeService', () => {
+describe('PaymentMethodTypeService', () => {
   let service: PaymentMethodTypeService;
+  let testingController: HttpTestingController;
+  const baseUrl = 'http://192.168.0.21:8000/api/payment-method-types';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(PaymentMethodTypeService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be able to get all payment method types', () => {
+    service.getAll().subscribe(pmts => {
+      expect(pmts).toBeTruthy();
+      expect(pmts.length).toBe(2);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(Object.values(PaymentMethodTypes));
+  });
+
+  it('should be able to get payment method type by id', () => {
+    service.getById('1').subscribe(pmt => {
+      expect(pmt).toBeTruthy();
+      console.log(pmt)
+      expect(pmt.name).toBe('Cash');
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/1");
+    expect(mockReq.request.method).toBe('GET');
+    mockReq.flush(PaymentMethodTypes[1]);
+  });
+
+  it('should be able to create payment method type', () => {
+    const newBody = { name: 'Gift Card' };
+
+    service.create(newBody).subscribe(pmt => {
+      expect(pmt).toBeTruthy();
+      console.log(pmt)
+      expect(pmt.name).toBe(newBody.name);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl);
+    expect(mockReq.request.method).toBe('POST');
+    const response = {
+      ...PaymentMethodTypes[1],
+      name: newBody.name,
+      id: 3
+    }
+    mockReq.flush(response);
+  });
+
+  it('should be able to update payment method type by id', () => {
+    const changes = { name: 'Credit Card' };
+
+    service.update('2', changes).subscribe(pmt => {
+      expect(pmt).toBeTruthy();
+      console.log(pmt)
+      expect(pmt.name).toBe(changes.name);
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/2");
+    expect(mockReq.request.method).toBe('PUT');
+    const response = PaymentMethodTypes[2];
+    response.name = changes.name;
+    mockReq.flush(response);
+  });
+
+  it('should be able to delete payment method type by id', () => {
+    service.delete('1').subscribe(pmt => {
+      expect(pmt).toBeFalsy();
+    });
+
+    const mockReq = testingController.expectOne(baseUrl + "/1");
+    expect(mockReq.request.method).toBe('DELETE');
+    mockReq.flush('');
   });
 });

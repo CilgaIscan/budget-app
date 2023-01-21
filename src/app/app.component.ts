5c25e1168af3d7d9fd760692604853959ea107ae
cilga <cilgaiscan@gmail.com>
2023-01-21 22:30:05 +0100

    Change page lang when btn clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Budget App';
+  
+  public changeLanguage(code: string) {
+    localStorage.setItem('locale', code);
+    window.location.reload();
+  }
 }
